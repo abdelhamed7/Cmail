177ccc3f57d5cd3311f578b47834dd71b86ffb78
Da34y <abdelhamedadel5@gmail.com>
2021-04-30 19:32:39 +0200

    fCommit

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const express=require('express');
-const app=express();
-app.get('/',(req,res)=>{
-    res.send({hi:'u'})
-});
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./Config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+require("./models/User");
+require("./Services/Passport");
 
-const PORT=process.env.PORT||5000;
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000 * 60,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+require("./Routes/authRoutes")(app);
+mongoose.connect(keys.mongoURI);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
